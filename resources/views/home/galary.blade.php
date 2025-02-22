dc6ca2f72d402cb6ef5cec6f58db1a0e459dc46a
fiahayatulaqilla <fiahayatulaqilla@gmail.com>
2025-02-22 18:01:13 +0700

    hotel

---
+++ resources/views/home/galary.blade.php
@@ -0,0 +1,103 @@
+<div  class="gallery">
+    <div class="container">
+       <div class="row">
+          <div class="col-md-12">
+             <div class="titlepage">
+                <h2>gallery</h2>
+             </div>
+          </div>
+       </div>
+       <div class="row">
+          <div class="col-md-3 col-sm-6">
+             <div class="gallery_img">
+                <figure><img src="images/gallery1.jpg" alt="#"/></figure>
+             </div>
+          </div>
+          <div class="col-md-3 col-sm-6">
+             <div class="gallery_img">
+                <figure><img src="images/gallery2.jpg" alt="#"/></figure>
+             </div>
+          </div>
+          <div class="col-md-3 col-sm-6">
+             <div class="gallery_img">
+                <figure><img src="images/gallery3.jpg" alt="#"/></figure>
+             </div>
+          </div>
+          <div class="col-md-3 col-sm-6">
+             <div class="gallery_img">
+                <figure><img src="images/gallery4.jpg" alt="#"/></figure>
+             </div>
+          </div>
+          <div class="col-md-3 col-sm-6">
+             <div class="gallery_img">
+                <figure><img src="images/gallery5.jpg" alt="#"/></figure>
+             </div>
+          </div>
+          <div class="col-md-3 col-sm-6">
+             <div class="gallery_img">
+                <figure><img src="images/gallery6.jpg" alt="#"/></figure>
+             </div>
+          </div>
+          <div class="col-md-3 col-sm-6">
+             <div class="gallery_img">
+                <figure><img src="images/gallery7.jpg" alt="#"/></figure>
+             </div>
+          </div>
+          <div class="col-md-3 col-sm-6">
+             <div class="gallery_img">
+                <figure><img src="images/gallery8.jpg" alt="#"/></figure>
+             </div>
+          </div>
+       </div>
+    </div>
+ </div>
+ <div  class="blog">
+    <div class="container">
+       <div class="row">
+          <div class="col-md-12">
+             <div class="titlepage">
+                <h2>Blog</h2>
+                <p>Lorem Ipsum available, but the majority have suffered </p>
+             </div>
+          </div>
+       </div>
+       <div class="row">
+          <div class="col-md-4">
+             <div class="blog_box">
+                <div class="blog_img">
+                   <figure><img src="images/blog1.jpg" alt="#"/></figure>
+                </div>
+                <div class="blog_room">
+                   <h3>Bed Room</h3>
+                   <span>The standard chunk </span>
+                   <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generatorsIf you are   </p>
+                </div>
+             </div>
+          </div>
+          <div class="col-md-4">
+             <div class="blog_box">
+                <div class="blog_img">
+                   <figure><img src="images/blog2.jpg" alt="#"/></figure>
+                </div>
+                <div class="blog_room">
+                   <h3>Bed Room</h3>
+                   <span>The standard chunk </span>
+                   <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generatorsIf you are   </p>
+                </div>
+             </div>
+          </div>
+          <div class="col-md-4">
+             <div class="blog_box">
+                <div class="blog_img">
+                   <figure><img src="images/blog3.jpg" alt="#"/></figure>
+                </div>
+                <div class="blog_room">
+                   <h3>Bed Room</h3>
+                   <span>The standard chunk </span>
+                   <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generatorsIf you are   </p>
+                </div>
+             </div>
+          </div>
+       </div>
+    </div>
+ </div>
